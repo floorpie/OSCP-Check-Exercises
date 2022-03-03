3753bdefabeb19ce151548527d12cdee1533a59e
floorpie <55995159+floorpie@users.noreply.github.com>
2022-03-03 20:25:13 +0100

    Update OSCP-Check-Exercises.py

--- OSCP-Check-Exercises.py
+++ OSCP-Check-Exercises.py
@@ -1,7 +1,7 @@
 # Forked from original mushroom-hat script - https://github.com/mushroom-hat/OSCP-Check-Exercises
+# All credit to mushroom-hat
 #
 # The exercise numbers WILL BE updated to reflect the exercise numbering used in the Offensive Security Training Library (Learn One subscription)
-#
 # floorp.ie - 03032022
 
 import docx2txt
